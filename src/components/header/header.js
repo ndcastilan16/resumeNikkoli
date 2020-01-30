da09c5d232d5ea61c929d1742e3cdbed499c3908
nikkolic <nikkolic@gentrack.com>
2020-01-31 11:01:22 +1300

    removed unused headers

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -27,21 +27,6 @@ export default class Header extends Component {
                   Resume
                 </a>
               </li>
-              <li>
-                <a className="smoothscroll" href="#portfolio">
-                  Works
-                </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#testimonials">
-                  Testimonials
-                </a>
-              </li>
-              <li>
-                <a className="smoothscroll" href="#contact">
-                  Contact
-                </a>
-              </li>
             </ul>{" "}
             {/* end #nav */}
           </nav>{" "}

fa5177b594ea2769a6f82824b6d4cdb52582cc9d
ndcastilan16 <59216935+ndcastilan16@users.noreply.github.com>
2020-01-30 23:10:56 +1300

    Update resumeData.js

--- src/components/resume/resumeData.js
+++ src/components/resume/resumeData.js
@@ -1,6 +1,6 @@
 let resumeData = {
     "imagebaseurl":"resumeNiks",
-    "name": "Nikkoli dfsdCastilan",
+    "name": "Nikkoli Castilan",
     "role": "Frontend Developer"
     }
   export default resumeData
